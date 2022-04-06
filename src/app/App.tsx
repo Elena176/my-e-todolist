d6d0c44c3fbd6cfcaf7944f622ba6801cb21b937
Elena176 <lena.tdn176@gmail.com>
2022-04-06 19:16:41 +0300

    add .

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -56,13 +56,12 @@ function App({demo = false}: PropsType) {
           <Route path="/" element={<TodolistsList demo={demo}/>}/>
           <Route path="/my-e-todolist" element={<TodolistsList demo={demo}/>}/>
           <Route path="/login" element={<Login/>}/>
-          <Route path="/404" element={<h1 style={{textAlign: 'center'}}>404 PAGE NOT FOUND</h1>}/>
+          <Route path="/404" element={<h1 style={{textAlign: 'center'}}>404 PAGE NOT FOUND.</h1>}/>
           <Route path="*" element={<Navigate to="/404"/>}/>
         </Routes>
       </Container>
     </div>
   )
 }
-
 
 export default App
