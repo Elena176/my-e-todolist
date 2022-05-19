4da28120b159c64f39ec79c92f06198f292b06c7
Elena176 <lena.tdn176@gmail.com>
2022-05-19 14:41:43 +0300

    added app-reducer.test.ts

---
+++ src/app/app-reducer.test.ts
@@ -0,0 +1,27 @@
+import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC, setIsInitializedAC} from './app-reducer';
+import {requestStatus} from '../enum/requestStatus';
+
+let startState: InitialStateType;
+
+beforeEach(() => {
+  startState = {
+    status: requestStatus.idle,
+    error: null,
+    isInitialized: false
+  }
+})
+
+test('correct error message should be set', () => {
+  const endState = appReducer(startState, setAppErrorAC({error: 'some error'}))
+  expect(endState.error).toBe('some error')
+})
+
+test('correct status should be set', () => {
+  const endState = appReducer(startState, setAppStatusAC({status: requestStatus.loading}))
+  expect(endState.status).toBe(requestStatus.loading)
+})
+
+test('app should be initialized', () => {
+  const endState = appReducer(startState, setIsInitializedAC({isInitialized: true}))
+  expect(endState.isInitialized).toBe(true)
+})
